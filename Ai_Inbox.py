b060f4198da111f752689b87c5c7d321dfaeccd4
Lyndon King Obenza <zobenza115@gmail.com>
2025-01-03 04:52:33 +0800

    produced new model but cant produce evaluation gpu keeps getting OOM

--- Ai_Inbox.py
+++ Ai_Inbox.py
@@ -5,6 +5,7 @@ import librosa
 from tensorflow.image import resize
 import tempfile
 import os
+from pathlib import Path
 
 
 #load model function
@@ -59,16 +60,93 @@ st.sidebar.title("Dashboard")
 app_page = st.sidebar.selectbox("Select Page",["Home","About Project","Try AI inbox"])
 
 
-if(app_page == "Home"):
-    st.header('''Welcome to AI Inbox, A Music Genre Classification system!''')
+if app_page == "Home":
+    st.header("Welcome to AI Inbox, A Music Genre Classification system!")
+    st.markdown("""
+    **AI Inbox** is an innovative music genre classification system powered by artificial intelligence. 
+    Upload your audio file, and let our model predict the genre of your song!
+
+    ### How it works:
+    1. Upload an audio file in .mp3 format.
+    2. Click on the 'Play Audio' button to listen to your file.
+    3. Click 'Predict Genre' to see the genre of the song.
+
+    It's that simple! Try it now and let AI Inbox classify your song's genre!
+    """)
     
+
+
+elif app_page == "About Project":
+    group_members = [
+    {
+        "name": "Dexter C. Pacana",
+        "role": "Video Documentation and Editing Lead",
+        "image_path":"./Members/member1.webp",
+    },
+    {
+        "name": "Renie Boy M. Maglinte",
+        "role": "Documentation Specialist",
+        "image_path":"./Members/member2.webp",
+    },
+    {
+        "name": "Lyndon King L. Obenza",
+        "role": "Developer",
+        "image_path":"./Members/member3.webp",
+    }
+    
+]
+    st.header("About AI Inbox")
+    st.markdown("""
+    **AI Inbox** leverages machine learning techniques to classify songs into various genres. 
+    This system was trained on a large dataset of music, and it uses Mel spectrogram features to analyze the audio and determine its genre. 
+    The goal is to help music lovers and creators categorize and identify songs with ease.
+
+    ### Technical Details:
+    - The model is built using Convolutional Neural Networks (CNNs) and trained on a collection of Mel spectrograms from 10 music genres.
+    -This  genres are blues, classical, country, disco, hiphop, jazz, metal, pop, reggae, and rock
+    - We preprocess the audio files into Mel spectrograms and resize them to fit the model's input requirements.
+
+    ### Libraries & Frameworks:
+    - **TensorFlow**: Provides powerful deep learning capabilities for model building and prediction.
+    - **Librosa**: Used for audio feature extraction and preprocessing.
+    - **Streamlit**: for this interactive web application.
+    
+    ### Version Used:
+    - **Python**:3.10.16
+    - **tensorflow**:2.10.0
+    - **scikit-learn**:1.3.0
+    - **numpy**:1.24.3
+    - **matplotlib**:3.7.2
+    - **seaborn**:0.13.0
+    - **pandas**:2.1.0
+    - **streamlit**:1.22.0
+    - **librosa**:0.10.1
+
+    ### Credits:
+    - This project was made possible by open-source dataset and libraries,the GTZAN dataset and the Librosa library.
+    - Special thanks to the TensorFlow community for their valuable resources.
+    - Youtube channels who became our guide for this project to be completed such as but not limited to IBM Technology,SPOTLESS TECH,James Briggs,Aladdin Persson,codebasics and others.
+    
+    """)
+    
+    # Display member details
+    for member in group_members:
+        col1, col2 = st.columns([1, 3])  
+
+        with col1:
+            st.image(member["image_path"], caption=member["name"], use_column_width=True)
+        
+        with col2:
+            st.subheader(member["name"])
+            st.write(f"**Role:** {member['role']}")
+        
     
 
 
 # AI INBOX
 elif(app_page == "Try AI inbox"):
     st.header("Model Prediction")
-    mp3_file = st.file_uploader("Upload your Mp3 file here and AI Inbox will try to guess its genre", type=["mp3"])
+    mp3_file = st.file_uploader("Upload your Mp3 file here and AI Inbox will try to guess its genre(Note:genres are limited to 10 genres, see about us for the genre list available)", type=["mp3"])
     
     if mp3_file is not None:
         # Save the file to a temporary directory
